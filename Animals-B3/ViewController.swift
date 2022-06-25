1ea0134e4f2728ff14f21347773358f7f61eb281
SaiKiran Panuganti <saikiranpanuganti92@gmail.com>
2022-06-25 16:16:54 +0530

    Initial Commit

---
+++ Animals-B3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animals-B3
+//
+//  Created by Saikiran Panuganti on 25/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
