a488b9375cd9ff533989b9b3635786c48ac94c01
SaiKiran Panuganti <saikiranpanuganti92@gmail.com>
2022-07-02 15:35:45 +0530

    Annotation animation done

--- Animals-B3/Locations/AnimalAnnotationView.swift
+++ Animals-B3/Locations/AnimalAnnotationView.swift
@@ -9,11 +9,14 @@ import Foundation
 import MapKit
 
 class AnimalAnnotationView: MKAnnotationView {
+    var animatingView: UIView?
     
     init(annotation: MKAnnotation?, reuseIdentifier: String?, image: String) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
         
         addBackgroundAndImage(image: image)
+        animateBorderRadius()
+        animateBorderColor()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -21,14 +24,43 @@ class AnimalAnnotationView: MKAnnotationView {
     }
     
     func addBackgroundAndImage(image: String) {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        let view = UIView(frame: CGRect(x: -50, y: -50, width: 100, height: 100))
         addSubview(view)
+        
+        let animatingView = UIView(frame: CGRect(x: 25, y: 25, width: 50, height: 50))
+        animatingView.layer.borderWidth = 2.0
+        animatingView.layer.borderColor = UIColor(named: "AppColor_Orange")?.cgColor
+        animatingView.layer.cornerRadius = 25
+        view.addSubview(animatingView)
+        
+        self.animatingView = animatingView
         
         let imageView = UIImageView(frame: CGRect(x: 25, y: 25, width: 50, height: 50))
         imageView.image = UIImage(named: image)
         imageView.layer.borderWidth = 2.0
         imageView.layer.borderColor = UIColor(named: "AppColor_Orange")?.cgColor
         imageView.layer.cornerRadius = 25
+        imageView.clipsToBounds = true
         view.addSubview(imageView)
+    }
+    
+    func animateBorderColor() {
+        let animation = CABasicAnimation(keyPath: "borderColor")
+        animation.fromValue = UIColor(named: "AppColor_Orange")?.cgColor
+        animation.toValue = UIColor(named: "AppColor_Orange_Zero")?.cgColor
+        animation.duration = 1.2
+        animatingView?.layer.add(animation, forKey: "borderColor")
+        animatingView?.layer.borderColor = UIColor(named: "AppColor_Orange_Zero")?.cgColor
+    }
+    
+    func animateBorderRadius() {
+        UIView.animate(withDuration: 1.2) {
+            self.animatingView?.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        } completion: { bool in
+            self.animatingView?.transform = .identity
+            self.animatingView?.layer.borderColor = UIColor(named: "AppColor_Orange")?.cgColor
+            self.animateBorderRadius()
+            self.animateBorderColor()
+        }
     }
 }
